208585674803820c494b1f718f3d549cbaa64b54
Nikhil R <nikhil@traceable.ai>
2023-11-11 15:53:22 +0530

    Migrate to lazy.nvim

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,12 +1,3 @@
--- Refresh init.lua and run PackerCompile when saved
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost init.lua source <afile> | PackerCompile
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
 vim.cmd([[
   au BufNewFile,BufRead Jenkinsfile setf groovy
 ]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,210 +1,134 @@
--- Bootstrapping if not installed
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-end
-
 -- returns the require for use in `config` parameter of packer's use
 -- expects the name of the config file
 local function get_config(name)
-  return string.format('require("config/%s")', name)
+  return function()
+    require("config/" .. name)
+  end
 end
 
-return require("packer").startup(
-  function(use)
-    -- Packer can manage itself
-    use "wbthomason/packer.nvim"
-    -- Change surround tags faster
-    use "tpope/vim-repeat"
-    use "tpope/vim-surround"
-    -- Change Root directory
-    use {
-      "ahmedkhalf/project.nvim",
-      config = get_config("project")
-    }
-    -- Snippets
-    use "rafamadriz/friendly-snippets"
-    -- Speeds up neovim
-    use "lewis6991/impatient.nvim"
-    -- Learn Vim Game
-    use "ThePrimeagen/vim-be-good"
-    -- Markdown Preview
-    use {
-      "ellisonleao/glow.nvim",
-      config = get_config("glow")
-    }
-    use "davidgranstrom/nvim-markdown-preview"
-    -- Terraform SyntaxHighlight
-    use "hashivim/vim-terraform"
+return {
+  -- Change surround tags faster
+  "tpope/vim-repeat",
+  "tpope/vim-surround",
+  -- Snippets
+  "rafamadriz/friendly-snippets",
+  -- Learn Vim Game
+  "ThePrimeagen/vim-be-good",
+  "davidgranstrom/nvim-markdown-preview",
+  -- Terraform SyntaxHighlight
+  "hashivim/vim-terraform",
+  -- Pictograms for LSP completion
+  "onsails/lspkind.nvim",
+  -- Change Root directory
+  { "ahmedkhalf/project.nvim",             config = get_config("project"),          main = "project_nvim" },
+  -- Markdown Preview
+  { "ellisonleao/glow.nvim",               config = get_config("glow") },
+  -- Key bindings pop-up
+  { "folke/which-key.nvim",                config = get_config("which-key") },
+  -- Notifications
+  { "rcarriga/nvim-notify",                config = get_config("notify") },
+  -- Indent blank lines (just like the name)
+  { "lukas-reineke/indent-blankline.nvim", config = get_config("indent_blankline"), main = "ibl" },
+  -- Color highlighter
+  { "norcalli/nvim-colorizer.lua",         config = get_config("colorizer") },
+  -- Smooth Scrolling
+  { "karb94/neoscroll.nvim",               config = get_config("neoscroll") },
+  -- Material Theme
+  { "marko-cerovac/material.nvim",         config = get_config("material") },
+  -- Commenter
+  { "numToStr/Comment.nvim",               config = get_config("comment") },
+  -- Tree to view symbols in source code
+  { "simrat39/symbols-outline.nvim",       config = get_config("outline") },
+  -- Automatic Bracket Closing
+  { "windwp/nvim-autopairs",               config = get_config("autopairs") },
+  -- Remove trailing spaces
+  { "lewis6991/spaceless.nvim",            config = get_config("spaceless") },
+  -- LSP
+  { "neovim/nvim-lspconfig",               config = get_config("lsp") },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "michaelb/sniprun",
+    build = "bash ./install.sh",
+    config = get_config("sniprun")
+  },
+  -- Dashboard
+  {
+    "goolord/alpha-nvim",
+    dependencies = {
+      "kyazdani42/nvim-web-devicons"
+    },
+    lazy = false,
+    config = get_config("alpha")
+  },
+  -- Git decorations that's show changed lines
+  {
+    "lewis6991/gitsigns.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = get_config("gitsigns")
+  },
+  -- Awesome Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = get_config("lualine"),
+  },
+  -- Code Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip-integ",
+      "f3fora/cmp-spell",
+      "octaltree/cmp-look",
+      "hrsh7th/cmp-calc",
+      "hrsh7th/cmp-emoji",
 
-    use {
-      "michaelb/sniprun",
-      run = "bash ./install.sh",
-      config = get_config("sniprun")
-    }
+    },
+    config = get_config("cmp"),
+  },
+  -- Well, should I explain more?
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = get_config("treesitter"),
+  },
+  -- Show window with diagnostics
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  -- Better fuzzy search
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      "kyazdani42/nvim-web-devicons",
 
-    -- Key bindings pop-up
-    use {
-      "folke/which-key.nvim",
-      config = get_config("which-key")
-    }
+    },
+    config = get_config("telescope"),
+  },
+  -- File explorer
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = {
 
-    -- Notifications
-    use {
-      "rcarriga/nvim-notify",
-      config = get_config("notify")
-    }
-
-    -- Indent blank lines (just like the name)
-    use {
-      "lukas-reineke/indent-blankline.nvim",
-      config = get_config("indent_blankline")
-    }
-
-    -- Color highlighter
-    use {
-      "norcalli/nvim-colorizer.lua",
-      config = get_config("colorizer")
-    }
-
-    -- Smooth Scrolling
-    use {
-      "karb94/neoscroll.nvim",
-      config = get_config("neoscroll")
-    }
-
-    -- Dashboard
-    use {
-      "goolord/alpha-nvim",
-      requires = { "kyazdani42/nvim-web-devicons" },
-      config = get_config("alpha")
-    }
-
-    -- Material Theme
-    use {
-      "marko-cerovac/material.nvim",
-      config = get_config("material")
-    }
-
-    -- Git decorations that's show changed lines
-    use {
-      'lewis6991/gitsigns.nvim',
-      requires = {
-        'nvim-lua/plenary.nvim'
-      },
-      config = get_config("gitsigns")
-    }
-
-    use({
-      "kdheepak/lazygit.nvim",
-      -- optional for floating window border decoration
-      requires = {
-        "nvim-lua/plenary.nvim",
-      },
-    })
-
-    -- Commenter
-    use {
-      'numToStr/Comment.nvim',
-      config = get_config('comment')
-    }
-
-    -- Tree to view symbols in source code
-    use {
-      "simrat39/symbols-outline.nvim",
-      config = get_config("outline"),
-    }
-
-    -- Automatic Bracket Closing
-    use {
-      "windwp/nvim-autopairs",
-      config = get_config("autopairs")
-    }
-
-    -- Awesome Statusline
-    use {
-      "nvim-lualine/lualine.nvim",
-      requires = { "kyazdani42/nvim-web-devicons", opt = true },
-      config = get_config("lualine")
-    }
-
-    -- LSP
-    use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
-
-    -- Pictograms for LSP completion
-    -- use({ "onsails/lspkind.nvim", requires = { { "famiu/bufdelete.nvim" } } })
-    use({ "onsails/lspkind.nvim" })
-
-    -- Code Completion
-    use({
-      "hrsh7th/nvim-cmp",
-      requires = {
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-nvim-lua" },
-        { "hrsh7th/cmp-buffer" },
-        { "hrsh7th/cmp-path" },
-        { "hrsh7th/cmp-cmdline" },
-        { "hrsh7th/vim-vsnip" },
-        { "hrsh7th/cmp-vsnip" },
-        { "hrsh7th/vim-vsnip-integ" },
-        { "f3fora/cmp-spell" },
-        { "octaltree/cmp-look" },
-        { "hrsh7th/cmp-calc" },
-        { "hrsh7th/cmp-emoji" },
-      },
-      config = get_config("cmp"),
-    })
-
-    -- Remove trailing spaces
-    use {
-      'lewis6991/spaceless.nvim',
-      config = get_config('spaceless')
-    }
-
-    -- Well, should I explain more?
-    use {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-      config = get_config("treesitter")
-    }
-
-    -- Show window with diagnostics
-    use {
-      "folke/trouble.nvim",
-      requires = {
-        { "nvim-tree/nvim-web-devicons" }
-      }
-    }
-
-    -- Better fuzzy search
-    use {
-      "nvim-telescope/telescope.nvim",
-      requires = {
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope-live-grep-args.nvim" },
-        { "kyazdani42/nvim-web-devicons", opt = true }
-      },
-      config = get_config("telescope")
-    }
-
-    -- For Formatting and a few other stuff
-    -- use {
-    --     "jose-elias-alvarez/null-ls.nvim",
-    --     requires = {
-    --         'nvim-lua/plenary.nvim'
-    --     },
-    --     config = get_config("null-ls")
-    -- }
-
-    -- File explorer
-    use {
-      "kyazdani42/nvim-tree.lua",
-      requires = {
-        "kyazdani42/nvim-web-devicons",
-        opt = true
-      },
-      config = get_config("nvim-tree")
-    }
-  end
-)
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = get_config("nvim-tree"),
+  }
+}
